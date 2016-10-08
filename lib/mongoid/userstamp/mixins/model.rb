ea7c318989c1ddc5b8858a391fcf2d5b90009f56
Tomas Hruska <hruska@btl.cz>
2016-10-08 21:22:51 +0200

    Fix for missing method

--- lib/mongoid/userstamp/mixins/model.rb
+++ lib/mongoid/userstamp/mixins/model.rb
@@ -17,6 +17,23 @@ module Userstamp
 
       protected
 
+      def self.belongs_to_record(association_name, options={})
+        association_class = options[:class_name] || association_name.to_s.singularize.classify
+        class_eval %<
+        field :#{association_name}_id, type: Integer
+        index(#{association_name}_id: 1)
+
+        def #{association_name}
+          @#{association_name} ||= #{association_class}.where(id: #{association_name}_id).first if #{association_name}_id
+        end
+
+        def #{association_name}=(object)
+          @#{association_name} = object
+          self.#{association_name}_id = object.try :id
+        end
+      >
+      end
+
       def set_created_by
         current_user = Mongoid::Userstamp.current_user(self.class.mongoid_userstamp_config.user_model)
         return if current_user.blank? || self.send(self.class.mongoid_userstamp_config.created_name)
